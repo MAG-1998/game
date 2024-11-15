bd576c8c953b50d2ed3678a604af00574e85e33b
MAG-1998 <bearded.sempai@gmail.com>
2024-11-16 02:47:02 +0500

    1

--- actions/actions.go
+++ actions/actions.go
@@ -1,46 +1,71 @@
 package actions
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
-	
 )
 
 var randsource = rand.NewSource(time.Now().UnixNano())
+
 var rangenerator = rand.New(randsource)
-var Currentmonsterhealth = 100
-var Currentplayerhealth = 100
+var Currentmonsterhealth = MONSTER_HEALTH
+var Currentplayerhealth = PLAYER_HEALTH
 
 func AttackMonster(isspecialattack bool) {
-	minAttackValue:= 5
-	maxAttackValue:= 10
+	minAttackValue := PLAYER_ATTACK_MIN_DMG
+	maxAttackValue := PLAYER_ATTACK_MAX_DMG
 
 	if isspecialattack {
-		minAttackValue = 10
-		maxAttackValue = 20
+		minAttackValue = PLAYER_SPECIAL_ATTACK_MIN_DMG
+		maxAttackValue = PLAYER_SPECIAL_ATTACK_MAX_DMG
 	}
 	dmgvalue := generaterandombetween(minAttackValue, maxAttackValue)
-	Currentmonsterhealth -= dmgvalue // currentmonsterhealth = currentmonsterhealt - dmgvalue
-
-}
-
-func HealPlayer(healoption string) {
-	
-	
-	minhealvalue := 10
-	maxhealvalue := 20
-	healvalue := generaterandombetween(minhealvalue,maxhealvalue)
-	healthdiff := 100 - Currentplayerhealth
-	if healthdiff>=healvalue{
-		Currentplayerhealth+=healvalue
+	if Currentmonsterhealth <= dmgvalue {
+		Currentmonsterhealth = 0
 	} else {
-		Currentplayerhealth = 100
+		Currentmonsterhealth -= dmgvalue // currentmonsterhealth = currentmonsterhealt - dmgvalue
 	}
-	
 }
 
-func AttackPlayer() {
+func HealPlayer() {
 
+	healvalue := generaterandombetween(PLAYER_HEAL_MIN, PLAYER_HEAL_MAX)
+	healthdiff := PLAYER_HEALTH - Currentplayerhealth
+	if healthdiff >= healvalue {
+		Currentplayerhealth += healvalue
+	} else {
+		Currentplayerhealth = PLAYER_HEALTH
+	}
+}
+
+func AttackPlayer(isspecialattack bool) {
+	minAttackValue := MONSTER_ATTACK_MIN_DMG
+	maxAttackValue := MONSTER_ATTACK_MAX_DMG
+
+	if isspecialattack {
+		minAttackValue = MONSTER_SPECIAL_ATTACK_MIN_DMG
+		maxAttackValue = MONSTER_SPECIAL_ATTACK_MAX_DMG
+		for i := 0; i < 800; i++ {
+			fmt.Print("---Ботаник оказался не так уж и прост---получай пизды гопник---")
+
+		}
+		if (Currentmonsterhealth + 30) <= MONSTER_HEALTH {
+			Currentmonsterhealth += 30
+		} else {
+			Currentmonsterhealth = MONSTER_HEALTH
+		}
+	}
+	dmgvalue := generaterandombetween(minAttackValue, maxAttackValue)
+
+	if Currentplayerhealth <= dmgvalue {
+		Currentplayerhealth = LOSE_POINT
+	} else {
+		Currentplayerhealth -= dmgvalue
+	}
+}
+func Gethealthamounts() (int, int) {
+	return Currentplayerhealth, Currentmonsterhealth
 }
 
 func generaterandombetween(min int, max int) int {
--- main.go
+++ main.go
@@ -1,52 +1,62 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+
 	"github.com/MAG-1998/game/actions"
 	"github.com/MAG-1998/game/interaction"
-
 )
 
-var reader = bufio.NewReader(os.Stdin)
 var currentround = 0
 
 func main() {
 	startgame()
 
 	winner := "" //player || monster || draw
-
+	loser := ""
 	for winner == "" {
-		winner = executeround()
+		winner, loser = executeround()
 	}
 
-	endgame()
+	endgame(winner,loser)
 }
 
 func startgame() {
 	interaction.PrintGreeting()
 }
 
-func executeround() string {
-	fmt.Println("------------- Botanik HP:",actions.Currentmonsterhealth,"-----------------Gopnik HP:", actions.Currentplayerhealth)
+func executeround() (winner string,loser string) {
 	currentround++
+	var playerhealth int
+	var monsterhealth int
 	IsSpecialround := currentround%3 == 0
+	is777 := currentround%13 == 0
 
 	interaction.Showavailableactions(IsSpecialround)
 	userchoice := interaction.GetUserChoice(IsSpecialround)
-	
+
 	if userchoice == "1" {
 		fmt.Println("Пинай гада!")
-		actions.AttackMonster(IsSpecialround)
+		 actions.AttackMonster(IsSpecialround)
 	} else if userchoice == "2" {
 		fmt.Println("Ням Ням")
+		 actions.HealPlayer()
 	} else if userchoice == "3" {
-actions.AttackMonster(IsSpecialround)
-fmt.Println("Хеликоптер---Хеликоптер")
+		actions.AttackMonster(IsSpecialround)
+		fmt.Println("Хеликоптер---Хеликоптер")
 	}
+	actions.AttackPlayer(is777)
+	playerhealth, monsterhealth = actions.Gethealthamounts()
+	fmt.Printf("================HP Гопника : %v============= HP Ботаника: %v================", playerhealth, monsterhealth)
 
-	return ""
+	if playerhealth <=0 {
+		return "Ботаник", "Гопник"
+	} else if monsterhealth<=0 {
+		return "Гопник", "Ботаник"
+	}
+	return "",""
 }
 
-func endgame() {}
+func endgame(winner string, loser string) {
+	interaction.Declarewinner(winner, loser)
+}
