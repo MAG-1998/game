8a775a351247f146c3eb66584df5d296d09f0396
MAG-1998 <bearded.sempai@gmail.com>
2024-11-17 05:42:31 +0500

    finall

--- interaction/input.go
+++ interaction/input.go
@@ -6,37 +6,35 @@ import (
 	"os"
 	"strings"
 )
+
 var reader = bufio.NewReader(os.Stdin)
 
-
-func GetUserChoice(Specialattackisavailable bool) string{
+func GetUserChoice(Specialattackisavailable bool) string {
 	for {
-	Playerchoice, _ := GetPlayerInput()
-		if Playerchoice == "1"{
+		Playerchoice, _ := GetPlayerInput()
+		if Playerchoice == "1" {
 			return "1"
-		} else if Playerchoice == "2"{
+		} else if Playerchoice == "2" {
 			return "2"
 		} else if Playerchoice == "3" && Specialattackisavailable {
 			return "3"
-		} else if Playerchoice=="3" && !Specialattackisavailable {
+		} else if Playerchoice == "3" && !Specialattackisavailable {
 			fmt.Print("\nнедостаточно сигма\n")
-			
+
 		} else {
 			fmt.Print("\nнорм вещи пиши э!\n")
 		}
-		}
-}	
-	
-	
+	}
+}
 
-func GetPlayerInput() (string,error) {
-	fmt.Println("Выбор за тобой юный гопник: ")
+func GetPlayerInput() (string, error) {
+	fmt.Print("Выбор за тобой юный гопник: ")
 
 	userInput, err := reader.ReadString('\n')
 	if err != nil {
 		return "", err
 	}
 
-	userInput=strings.TrimSpace(userInput)
+	userInput = strings.TrimSpace(userInput)
 	return userInput, nil
 }
